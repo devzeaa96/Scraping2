a58d020fcfdec0c3b253a151ca2325278cc4aeae
David Zea <desarrollador1.ti@globalsac.co>
2020-07-22 19:39:35 -0500

    new files

---
+++ scrap.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://resultados.as.com/resultados/futbol/primera/clasificacion/'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#Equipos
+
+eq = soup.find_all('span', class_='nombre-equipo')
+
+equipos = list()
+count = 0
+for i in eq:
+    if count < 20:
+        equipos.append(i.text)
+    else:
+        break
+    count += 1
+
+#puntos
+
+pt = soup.find_all('td', class_='destacado')
+puntos = list()
+count = 0
+for a in pt:
+    if count < 20:
+        puntos.append(a.text)
+    else:
+        break
+    count += 1
+
+df = pd.DataFrame({'Nombres': equipos, 'Puntos':puntos}, index=list(range(1,21)))
+
+df.to_csv('Clasificacion', index=False)
